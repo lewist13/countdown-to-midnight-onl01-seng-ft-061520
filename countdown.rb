9ba4ea009291fcce73e57d1171111d68cd3fdd04
TaeVon Lewis <lewistpro@gmail.com>
2020-06-17 15:52:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,17 +1,13 @@
-def countdown(x)
-while x > 0
-  puts "#{x} SECOND(S)!"
-  sleep 1
+x = 10
+while x == 0
   x -= 1
-end
-return "HAPPY NEW YEAR!"
+  puts "#{number} SECOND(S)!"
+
+
+def countdown
+  
+  
 end
 
-def countdown_with_sleep(x)
-  while x > 0
-  puts "#{x} SECOND(S)!"
-  sleep 1
-  x -= 1
-end
-return "HAPPY NEW YEAR!"
+returns "HAPPY NEW YEAR!"
 end
